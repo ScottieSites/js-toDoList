2097bcdf44bde872f566e9b0ff6ec248c5421eba
Anang Williams <anangwilliams@gmail.com>
2020-08-28 21:25:34 -0400

    first commit

---
+++ toDoList.js
@@ -0,0 +1,16 @@
+const newTask = document.querySelector('#list');
+
+const sendTask = document.querySelector('#submit');
+
+const savedTask = document.querySelector('#added task');
+
+sendTask.onclick = function (){
+const savedValue = newTask.value;
+
+console.log(savedValue);
+
+
+
+
+}
+
