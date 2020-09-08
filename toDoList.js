c5ee3645f6e881cfeb59c5929436a0dee4fe3afe
Anang Williams <anangwilliams@gmail.com>
2020-09-07 20:02:26 -0400

    Third commit

--- toDoList.js
+++ toDoList.js
@@ -1,16 +1,69 @@
-const newTask = document.querySelector('#list');
+const input = document.querySelector("#input");
+const ul = document.querySelector("ul");
+const submit = document.querySelector("#submit");
 
-const sendTask = document.querySelector('#submit');
+let lists = [];
 
-const savedTask = document.querySelector('#added task');
 
-sendTask.onclick = function (){
-const savedValue = newTask.value;
+submit.addEventListener('click', addItem);
 
-console.log(savedValue);
+ul.addEventListener('click', deleteList);
+
+function addItem(event){
+  event.preventDefault();
+
+  let input = document.getElementById('input');
+  let listItem =input.value
+
+  let li = document.createElement('li');
+  
+  li.appendChild(document.createTextNode(listItem));
+
+  ul.appendChild(li);
+
+  let deleteButton = document.createElement('button');
+  
+  deleteButton.className = 'delete'
+  
+  deleteButton.appendChild(document.createTextNode(" " + 'Remove'));
+  
+  li.appendChild(deleteButton);
+};
+
+function deleteList(event){
+if(event.target.classList.contains('delete')){
+if(confirm('Do you really want to delete this task?')){
+  let li = event.target.parentElement;
+  ul.removeChild(li);
+    }
+  }
+}
+
+if(window.localStorage){
+  let input = document.getElementById('input');
+input.value = localStorage.getItem('input');
+input.addEventListener('lists', function(){
+  localStorage.setItem('');
+})
+
 
 
 
 
 }
 
+// function addItem(){
+//   submit.addEventListener('click', addItem)
+// for (var i = 0; i < input.length; i++) 
+//     if (inputs[i].value.length)
+//       lists.push(inputs[i].value);
+//   console.log(input.value)
+//   displayList()
+// };
+// addItem()
+
+// function displayList(){
+// let newList = document.createElement('li'); 
+// newList.textContent = input.value; 
+// ul.appendChild(newList);
+// }
